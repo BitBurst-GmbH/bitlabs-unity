65944dc78c389515b88e7c5d85df551caf1d382c
omaRaad <omar.raad@bitburst.net>
2023-02-27 11:02:57 +0100

    Add SimpleWidget and modify SurveyContainer script to accomodate to it
    Related issues: BLD-1372

--- Assets/Plugins/Source/Scripts/SurveyContainer.cs
+++ Assets/Plugins/Source/Scripts/SurveyContainer.cs
@@ -1,26 +1,26 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SurveyContainer : MonoBehaviour
 {
+
+    private const string SimpleWidget = "SimpleWidget";
+    private const string CompactWidget = "CompactWidget";
+    private const string FullWidthWidget = "FullWidthWidget";
+
     [SerializeField] private GameObject prefab;
     [SerializeField] private Sprite fillStarImage;
 
+    private string rewardTextPath, playImagePath, loiTextPath, ratingTextPath;
+
     public void UpdateList(Survey[] surveys)
     {
-        if (ColorUtility.TryParseHtmlString(BitLabs.WidgetColor, out Color color))
-        {
-            prefab.GetComponent<Image>().color = color;
 
-            prefab.transform
-                .Find("RightPanel/RewardText")
-                .GetComponent<TMP_Text>().color = color;
+        UpdateGamePaths();
 
-            prefab.transform
-                .Find("RightPanel/PlayImage")
-                .GetComponent<Image>().color = color;
-        }
+        UpdateColors();
 
         GameObject surveyWidget;
 
@@ -35,24 +35,82 @@ public class SurveyContainer : MonoBehaviour
             surveyWidget.GetComponent<Button>().onClick.AddListener(SurveyOnClick);
             
             surveyWidget.transform
-                .Find("LeftPanel/TopPanel/LoiText")
-                .GetComponent<TMP_Text>().text = survey.loi + " minutes";
+                .Find(loiTextPath)
+                .GetComponent<TMP_Text>().text = GetLoi(survey.loi);
 
             surveyWidget.transform
-                .Find("LeftPanel/BottomPanel/RatingText")
-                .GetComponent<TMP_Text>().text = survey.rating.ToString();
+                .Find(rewardTextPath)
+                .GetComponent<TMP_Text>().text = GetReward(survey.cpi);
+
+            SetupRating(surveyWidget, survey.rating);
+        }
+    }
+
+    private string GetReward(string cpi)
+    {
+        return prefab.name switch
+        {
+            SimpleWidget => $"EARN {cpi}",
+            CompactWidget => $"EARN\n{cpi}",
+            _ => "",
+        };
+    }
+
+    private void SetupRating(GameObject surveyWidget, int rating)
+    {
+        if (prefab.name == SimpleWidget) return;
+
+        surveyWidget.transform
+                .Find(ratingTextPath)
+                .GetComponent<TMP_Text>().text = rating.ToString();
 
 
-            for (int i = 1; i <= survey.rating; i++)
-            {
-                surveyWidget.transform
-                    .Find($"LeftPanel/BottomPanel/Star{i}")
-                    .GetComponent<Image>().sprite = fillStarImage;
-            }
-
+        for (int i = 1; i <= rating; i++)
+        {
             surveyWidget.transform
-                .Find("RightPanel/RewardText")
-                .GetComponent<TMP_Text>().text = "EARN\n" + survey.cpi;
+                .Find($"LeftPanel/BottomPanel/Star{i}")
+                .GetComponent<Image>().sprite = fillStarImage;
+        }
+    }
+
+    private string GetLoi(double loi)
+    {
+        return prefab.name == SimpleWidget ? $"Now in {loi} minutes!" : $"{loi} minutes";
+    }
+
+    private void UpdateColors()
+    {
+        if (ColorUtility.TryParseHtmlString(BitLabs.WidgetColor, out Color color))
+        {
+            prefab.GetComponent<Image>().color = color;
+
+            if (prefab.name != CompactWidget) return;
+         
+            prefab.transform
+                .Find(rewardTextPath)
+                .GetComponent<TMP_Text>().color = color;
+
+            prefab.transform
+                .Find(playImagePath)
+                .GetComponent<Image>().color = color;
+            
+        }
+    }
+
+    private void UpdateGamePaths()
+    {
+        switch(prefab.name)
+        {
+            case SimpleWidget:
+                loiTextPath = "RightPanel/LoiText";
+                rewardTextPath = "RightPanel/RewardText";
+                break;
+            case CompactWidget:
+                playImagePath = "RightPanel/PlayImage";
+                rewardTextPath = "RightPanel/RewardText";
+                loiTextPath = "LeftPanel/TopPanel/LoiText";
+                ratingTextPath = "LeftPanel/BottomPanel/RatingText";
+                break;
         }
     }
 
